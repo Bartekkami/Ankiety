81fb000c7263a6b85670680522fce2b61e8deb43
Bartek <bartekkami@wp.pl>
2018-02-27 14:13:13 +0100

    chyba dobrze

--- ankietka/ankietka/urls.py
+++ ankietka/ankietka/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
-from apteczka import views as apteczka_views
+from django.urls import include, path
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^signup/$', apteczka_views.signup, name='signup'),
+    path('ankieter/', include('ankieter.urls')),
+
 ]
