2a192c6bd115282e3d81fa9fe5a2e8d0357305a5
Bartek <bartekkami@wp.pl>
2018-02-27 11:26:56 +0100

    singup.html

--- ankietka/apteczka/views.py
+++ ankietka/apteczka/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authienticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
 
-# Create your views here.
+def singup(request):
+    if request.method == 'POST'
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+       form.save()
+       username + form.cleaned_data.get('username')
+       raw_password = form.cleaned_data.get('password1')
+       user = authenticate(username=username, password=raw_password)
+       login(request, user)
+    else:
+       form = UserCreationForm()
+    return render(request, 'singup.html', {'form': form})
+
