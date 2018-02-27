16318bedb7dabfc23866360f713f35e70b1da668
Bartek <bartekkami@wp.pl>
2018-02-27 12:39:16 +0100

    nie tez

--- ankietka/ankietka/urls.py
+++ ankietka/ankietka/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from apteczka import views as apteczka_views
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^signup/$', apteczka_views.signup, name='signup'),
 ]
