bafe29863d38589a824598e54dd2728995ccf610
Bartek <bartekkami@wp.pl>
2018-03-13 14:30:02 +0100

    nwm

--- ankietka/ankieter/models.py
+++ ankietka/ankieter/models.py
@@ -5,7 +5,30 @@ from django.contrib.auth.models import User
 
 
 
+class Ankieta(models.Model):
+    nazwa_ankiety = models.CharField(max_length=30, default=None)
+    def __str__(self):
+        return self.nazwa_ankiety
+    class Meta:
+        verbose_name = 'Ankieta'
+        verbose_name_plural = 'Ankiety'
+
+
+
+class Grupa(models.Model):
+    ankieta = models.ForeignKey(Ankieta, on_delete=models.CASCADE, default=None)
+    nazwa_grupy = models.CharField(max_length=30, default=None)
+    def __str__(self):
+        return self.nazwa_grupy
+    class Meta:
+        verbose_name = 'Grupa'
+        verbose_name_plural = 'Grupy'
+
+
+
 class Pytanie(models.Model):
+    ankieta = models.ForeignKey(Ankieta, on_delete=models.CASCADE, default=None, blank=True)
+    grupa = models.ForeignKey(Grupa, on_delete=models.CASCADE, default=None, blank=True)
     pytanie_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     pytanie_plik = models.ImageField(default=None, blank=True, null=True)
